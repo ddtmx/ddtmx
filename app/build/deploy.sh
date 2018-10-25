816bded376dac22417a5f4c484de4d6342f6e3e7
ddtmx <ddtmx>
2018-10-25 18:35:00 -0500

    changes

--- app/build/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-SWARM_NAME;
-
-name=${SWARM_NAME#*_}    #prefix
-name=${name%*.yml} #suffix
-docker stack deploy -c $SWARM_NAME $name
--- ci/README.md
+++
@@ -1,5 +0,0 @@
-# CI strategies
-
-### check one by one and run from your Jenkins instance
-
-#### follow the setup to get docker run into jenkins
